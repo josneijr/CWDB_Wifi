65adf450ea017f7c1a23b941646fdbd57940faaa
Josnei Luis Olszewski Junior <josneijr@gmail.com>
2018-01-21 19:22:06 -0200

    Incluindo comunicação serial

--- sketch_jan15a/sketch_jan15a.ino
+++ sketch_jan15a/sketch_jan15a.ino
@@ -1,15 +1,23 @@
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
+#include <SoftwareSerial.h>
+
 
 const char* ssid = "dlink";//type your ssid
 const char* password = "2222222222";//type your password
 
 String mensagem = "";
+const int timeout = 1000;
 
 WiFiServer server(80);//Service Port
 
+SoftwareSerial STM32(D5,D6);
+
+
 void setup() {
+  STM32.begin(19200);
   Serial.begin(115200);
+  STM32.setTimeout(timeout);
   delay(10);
 
   // Connect to WiFi network
@@ -38,40 +46,47 @@ void setup() {
   Serial.println("/");
 }
 
-void ChegouDadosSerial(string dadosdaserial)
-{
-    mensagem += dadosdaserial;
-}
-
 void loop() {
 
   //a cada 3 minutos
+
   
   if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
 
+    while(STM32.available())
+    {
+    mensagem = STM32.readString();
+    //STM32.setTimeout(1000);
+    STM32.flush();
+     } 
+     
     HTTPClient http;    //Declare object of class HTTPClient
 
+    
+    //mensagem = STM32.readString();
     http.begin("http://cwdb.herokuapp.com/receiveData");      //Specify request destination
     http.addHeader("Content-Type", "application/json;charset=utf-8");  //Specify content-type header
 
     String message = "{";
 
     message += "}";
-  
-    int httpCode = http.POST("{\"timestamp\": \"15/01/2018 22:26:49\", \"lat\": \"-25.45\", \"long\": \"-49.29\", \"value\": \"67\"}");   //Send the request
+    //int httpCode = http.POST("{\"timestamp\": \"15/01/2018 22:26:49\", \"lat\": \"-25.45\", \"long\": \"-49.29\", \"value\": \"67\"}");   //Send the request
+    int httpCode = http.POST(mensagem);
+
+   
     String payload = http.getString();                  //Get the response payload
 
     Serial.println(httpCode);   //Print HTTP return code
     Serial.println(payload);    //Print request response payload
-
+    
     http.end();  //Close connection
-
+    Serial.println(mensagem);
+   // STM32.flush();
   }
   else {
      Serial.println("Error in WiFi connection");
   }
 
   delay(10000);  //Send a request every 30 seconds
-
-
 }
+
