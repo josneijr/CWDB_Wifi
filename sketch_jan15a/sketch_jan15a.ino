a5a1d7bda0e76e76b004de9c67f25b1209c8610d
Josnei Luis Olszewski Junior <josneijr@gmail.com>
2018-01-21 22:09:22 -0200

    Tudo funcionando, faltando apenas reconectar ao perder o sinal do Wi-fi

--- sketch_jan15a/sketch_jan15a.ino
+++ sketch_jan15a/sketch_jan15a.ino
@@ -2,91 +2,141 @@
 #include <ESP8266WiFi.h>
 #include <SoftwareSerial.h>
 
-
 const char* ssid = "dlink";//type your ssid
 const char* password = "2222222222";//type your password
 
-String mensagem = "";
-const int timeout = 1000;
+//Contador usado para andar no array
+int cont = 0;
+//Array contendo as últimas N mensagens completas
+String msgArray[100];
+//Buffer para mensagens incompletas
+String msgBuffer;
 
-WiFiServer server(80);//Service Port
+SoftwareSerial STM32(D5,D6,false,256);
 
-SoftwareSerial STM32(D5,D6);
+void setup() 
+{  
+    STM32.begin(19200);
+    Serial.begin(115200);
+    
+    STM32.setTimeout(1000);
+    delay(10);
 
-
-void setup() {
-  STM32.begin(19200);
-  Serial.begin(115200);
-  STM32.setTimeout(timeout);
-  delay(10);
-
-  // Connect to WiFi network
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
-
-  // Start the server
-  server.begin();
-  Serial.println("Server started");
-
-  // Print the IP address
-  Serial.print("Use this URL to connect: ");
-  Serial.print("http://");
-  Serial.print(WiFi.localIP());
-  Serial.println("/");
+    ConectaWifi();
 }
 
-void loop() {
-
-  //a cada 3 minutos
-
+void ConectaWifi()
+{
+    // Connect to WiFi network
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
   
-  if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
+    WiFi.begin(ssid, password);
+  
+    while (WiFi.status() != WL_CONNECTED) 
+    {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("");
+    Serial.println("WiFi connected");
+  
+    // Print the IP address
+    Serial.print("IP: ");
+    Serial.print(WiFi.localIP());  
+}
 
+void LerSerial()
+{
     while(STM32.available())
     {
-    mensagem = STM32.readString();
-    //STM32.setTimeout(1000);
-    STM32.flush();
-     } 
-     
-    HTTPClient http;    //Declare object of class HTTPClient
+        msgBuffer = STM32.readStringUntil('}');
+        STM32.readString();
 
+        Serial.println(msgBuffer);
+        msgArray[cont] = msgBuffer;
+
+        cont++;
+    }   
+}
+
+void EnviarWifi()
+{
+    //Criamos um objeto de conexão para esse cliente
+    HTTPClient http;
+
+    //URL para a qual vamos enviar
+    http.begin("http://cwdb.herokuapp.com/receiveData");
+    //Header define que vamos enviar um JSON
+    http.addHeader("Content-Type", "application/json;charset=utf-8");
+
+    String message = "[";
+
+    for(int i=0; i<cont; i++)
+    {
+        if(msgArray[i]==0)
+            return;
+      
+        if(i!=0)
+            message += ",";
+      
+        message += msgArray[i];
+
+        message += "}";
+    }
+  
+    message += "]";
+
+    Serial.println(message);
+
+    //Retorno do HTTP (200 = OK; qualquer outro número = erro)
+    int httpCode = http.POST(message);
+
+    //Se conseguiu enviar
+    if(httpCode == 200)
+    {
+        //Apagar o array
+        memset(msgArray, 0, sizeof(msgArray));    
+        cont = 0;      
+    }
+
+    //Pegar a resposta do payload
+    String payload = http.getString();
+
+    //Imprime o código HTTP de retorno
+    Serial.println(httpCode);
+    //Imprime a resposta de payload
+    Serial.println(payload);
+
+    //Fecha a comunicação do cliente
+    http.end();
+}
+
+void loop() 
+{
+    //Vamos ler a serial toda vez
+    LerSerial();
+
+    Serial.println("Loop");
     
-    //mensagem = STM32.readString();
-    http.begin("http://cwdb.herokuapp.com/receiveData");      //Specify request destination
-    http.addHeader("Content-Type", "application/json;charset=utf-8");  //Specify content-type header
+    //Wifi conectado?
+    if (WiFi.status() == WL_CONNECTED) 
+    {
+        //Vamos escrever na serial quando temos dados no array
+        if(cont>0)
+        {
+            EnviarWifi();
+        }
+    }
+    else 
+    {
+        Serial.println("Error in WiFi connection");
 
-    String message = "{";
+        WiFi.disconnect();
+        ConectaWifi();
+    }
 
-    message += "}";
-    //int httpCode = http.POST("{\"timestamp\": \"15/01/2018 22:26:49\", \"lat\": \"-25.45\", \"long\": \"-49.29\", \"value\": \"67\"}");   //Send the request
-    int httpCode = http.POST(mensagem);
-
-   
-    String payload = http.getString();                  //Get the response payload
-
-    Serial.println(httpCode);   //Print HTTP return code
-    Serial.println(payload);    //Print request response payload
-    
-    http.end();  //Close connection
-    Serial.println(mensagem);
-   // STM32.flush();
-  }
-  else {
-     Serial.println("Error in WiFi connection");
-  }
-
-  delay(10000);  //Send a request every 30 seconds
+    //Aguardar 1 segundo
+    delay(1000);  
 }
 
