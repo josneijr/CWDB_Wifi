f8c52709e3afa2b015a7f782b49b76aa8cf9c75d
Josnei Luis Olszewski Junior <josneijr@gmail.com>
2018-02-04 01:58:11 -0200

    Funcional
    Funcionando! Falta colocar a mensagem no formato certo e arrumar a aparência do código

--- sketch_jan15a/sketch_jan15a.ino
+++ sketch_jan15a/sketch_jan15a.ino
@@ -5,138 +5,70 @@
 const char* ssid = "dlink";//type your ssid
 const char* password = "2222222222";//type your password
 
-//Contador usado para andar no array
-int cont = 0;
-//Array contendo as últimas N mensagens completas
-String msgArray[100];
-//Buffer para mensagens incompletas
-String msgBuffer;
+SoftwareSerial STM32(D5, D6, false, 256);
 
-SoftwareSerial STM32(D5,D6,false,256);
-
-void setup() 
-{  
-    STM32.begin(19200);
-    Serial.begin(115200);
-    
-    STM32.setTimeout(1000);
-    delay(10);
-
-    ConectaWifi();
-}
-
-void ConectaWifi()
-{
-    // Connect to WiFi network
-    Serial.print("Connecting to ");
-    Serial.println(ssid);
-  
-    WiFi.begin(ssid, password);
-  
-    while (WiFi.status() != WL_CONNECTED) 
-    {
-        delay(500);
-        Serial.print(".");
-    }
-    Serial.println("");
-    Serial.println("WiFi connected");
-  
-    // Print the IP address
-    Serial.print("IP: ");
-    Serial.print(WiFi.localIP());  
-}
+String message = "";
 
 void LerSerial()
 {
-    while(STM32.available())
-    {
-        msgBuffer = STM32.readStringUntil('}');
-        STM32.readString();
+    Serial.println("Mensagem da serial: ");
+     while(STM32.available())
+     {
+          message = STM32.readStringUntil('\r');
+     }
 
-        Serial.println(msgBuffer);
-        msgArray[cont] = msgBuffer;
-
-        cont++;
-    }   
+     
+     Serial.println(message);
 }
 
-void EnviarWifi()
-{
-    //Criamos um objeto de conexão para esse cliente
-    HTTPClient http;
-
-    //URL para a qual vamos enviar
-    http.begin("http://cwdb.herokuapp.com/receiveData");
-    //Header define que vamos enviar um JSON
-    http.addHeader("Content-Type", "application/json;charset=utf-8");
-
-    String message = "[";
-
-    for(int i=0; i<cont; i++)
-    {
-        if(msgArray[i]==0)
-            return;
-      
-        if(i!=0)
-            message += ",";
-      
-        message += msgArray[i];
-
-        message += "}";
-    }
+void setup() {
+  STM32.begin(19200);
+  STM32.setTimeout(10000);
   
-    message += "]";
+  Serial.begin(115200);
+  delay(10);
 
-    Serial.println(message);
+  // Connect to WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
 
-    //Retorno do HTTP (200 = OK; qualquer outro número = erro)
-    int httpCode = http.POST(message);
+  WiFi.begin(ssid, password);
 
-    //Se conseguiu enviar
-    if(httpCode == 200)
-    {
-        //Apagar o array
-        memset(msgArray, 0, sizeof(msgArray));    
-        cont = 0;      
-    }
-
-    //Pegar a resposta do payload
-    String payload = http.getString();
-
-    //Imprime o código HTTP de retorno
-    Serial.println(httpCode);
-    //Imprime a resposta de payload
-    Serial.println(payload);
-
-    //Fecha a comunicação do cliente
-    http.end();
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
 }
 
-void loop() 
-{
-    //Vamos ler a serial toda vez
-    LerSerial();
+void loop() {
 
-    Serial.println("Loop");
-    
-    //Wifi conectado?
-    if (WiFi.status() == WL_CONNECTED) 
-    {
-        //Vamos escrever na serial quando temos dados no array
-        if(cont>0)
-        {
-            EnviarWifi();
-        }
-    }
-    else 
-    {
-        Serial.println("Error in WiFi connection");
+  LerSerial();
+  
+  if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
 
-        WiFi.disconnect();
-        ConectaWifi();
-    }
+    HTTPClient http;    //Declare object of class HTTPClient
 
-    //Aguardar 1 segundo
-    delay(1000);  
+    http.begin("http://cwdb.herokuapp.com/receiveData");      //Specify request destination
+    http.addHeader("Content-Type", "application/json;charset=utf-8");  //Specify content-type header
+  
+    int httpCode = http.POST("[{\"timestamp\": \"15/01/2018 22:26:49\", \"lat\": \"-25.45\", \"long\": \"-49.29\", \"value\": \"67\"}]");   //Send the request
+    String payload = http.getString();                  //Get the response payload
+
+    Serial.println(httpCode);   //Print HTTP return code
+    Serial.println(payload);    //Print request response payload
+
+    http.end();  //Close connection
+
+  }
+  else {
+     Serial.println("Error in WiFi connection");
+  }
+
+  delay(1000);  //Send a request every 30 seconds
+
+
 }
-
